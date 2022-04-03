366a98bf1515c30e3db354586d494f935d58bbfe
elizaBianca <eliza.manciu66@gmail.com>
2022-04-03 23:10:42 +0200

    business logic classes from business logic tier

---
+++ CSharp/BusinessLogicServer/Model/Order/IOrderModel.cs
@@ -0,0 +1,6 @@
+﻿namespace BusinessLogicServer.Model.Order;
+
+public interface IOrderModel
+{
+    public Task<List<ModelClasses.Order>> GetAllOrdersAsync();
+}
---
+++ CSharp/BusinessLogicServer/Model/Order/OrderModel.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogicServer.Networking.Order;
+
+namespace BusinessLogicServer.Model.Order;
+
+public class OrderModel : IOrderModel
+{
+    private IOrderNetworking networking;
+
+    public OrderModel(IOrderNetworking networking)
+    {
+        this.networking = networking;
+    }
+
+    public async Task<List<ModelClasses.Order>> GetAllOrdersAsync()
+    {
+       return await networking.GetAllOrdersAsync();
+    }
+}
