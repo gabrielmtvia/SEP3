fcecbc32331870389d332147479f079b5dcdcee6
elizaBianca <eliza.manciu66@gmail.com>
2022-04-03 23:10:02 +0200

    project start up class

--- Java/src/main/java/via/sep3/group2/DataServerApplication.java
+++ Java/src/main/java/via/sep3/group2/DataServerApplication.java
@@ -1,11 +1,11 @@
-package via.sep3.group2.rest.orderapi;
+package via.sep3.group2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderApiApplication {
+public class DataServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApiApplication.class, args);
+        SpringApplication.run(DataServerApplication.class, args);
     }
 }
