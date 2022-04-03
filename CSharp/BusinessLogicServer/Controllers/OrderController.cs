c89249949ce57e42d4b17ee5ad56f975ce7835d9
elizaBianca <eliza.manciu66@gmail.com>
2022-04-03 23:13:45 +0200

    REST order endpoint

---
+++ CSharp/BusinessLogicServer/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogicServer.Model.Order;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessLogicServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    private IOrderModel model;
+
+    public OrderController(IOrderModel model)
+    {
+        this.model = model;
+    }
+
+    [HttpGet]
+    public async Task<List<ModelClasses.Order>> GetAllOrdersAsync()
+    {
+        return await model.GetAllOrdersAsync();
+    }
+}
