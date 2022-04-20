7e1f5b9355bd968bb513ec388a82c77b7c07fc2c
hamoudi8080 <82207003+hamoudi8080@users.noreply.github.com>
2022-04-20 15:09:08 +0200

    Hamoudi test commitment

--- Java/src/main/java/via/sep3/group2/testCommitFromTomaszIntelliJ.java
+++ Java/src/main/java/via/sep3/group2/testCommitFromTomaszIntelliJ.java
@@ -4,6 +4,7 @@ public class testCommitFromTomaszIntelliJ {
   public static void main(String[] args) {
     System.out.println("Working!");
     System.out.println("Here's Hamoudi, it's working here too!");
+    System.out.println("Test branchHamoudi 1");
   }
 
 
