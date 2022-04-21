3884851292bdf77ffd9bceb4f82816dcb9203e73
hamoudi8080 <82207003+hamoudi8080@users.noreply.github.com>
2022-04-21 09:50:17 +0200

    the code has been cliened

--- CSharp/BlazorClient/Program.cs
+++ CSharp/BlazorClient/Program.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using BlazorClient.Data;
+
+
 using BlazorClient.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
 builder.Services.AddHttpClient<IOrderService, OrderService>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7031");
