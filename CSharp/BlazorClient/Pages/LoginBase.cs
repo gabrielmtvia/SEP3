af6ebdb90e3d5fabb3ba8f5af143e0f95c96b23a
hamoudi8080 <82207003+hamoudi8080@users.noreply.github.com>
2022-04-27 17:15:42 +0200

    new update

--- CSharp/BlazorClient/Pages/LoginBase.cs
+++ CSharp/BlazorClient/Pages/LoginBase.cs
@@ -1,8 +1,34 @@
 using Microsoft.AspNetCore.Components;
+using ModelClasses;
+
 
 namespace BlazorClient.Pages;
-
+ 
 public class LoginBase : ComponentBase
 {
-    
+
+    public string? userName { get; set; }
+    public string? password { get; set; }
+
+    //
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+    // public User User { get; set; }
+    // private string? errorLabel;
+    //
+    private async Task LoginAsync()
+    {
+       // errorLabel = "";
+        try
+        {
+         //   await authService.LoginAsync(userName, password);
+         NavigationManager.NavigateTo("/OrderList");
+        }
+        catch (Exception e)
+        {
+         //   errorLabel = $"Error: {e.Message}";
+        }
+    }
+
+
 }
