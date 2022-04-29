119d8d58e6a733027eccde89bc2349300e755029
hamoudi8080 <82207003+hamoudi8080@users.noreply.github.com>
2022-04-30 00:18:44 +0200

    validation made

--- CSharp/ModelClasses/User.cs
+++ CSharp/ModelClasses/User.cs
@@ -1,16 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ModelClasses;
 
 public class User
 {
-   
+    
+ 
+ 
+    [Required]
+    [StringLength(8, ErrorMessage = "UserName too long (8 characters limit).")]
     public string userName { get; set; }
-    public string password { get; set; }
-    public string name { get; set; }
-    public string role { get; set; }
-    public string address { get; set; }
-    public string phone { get; set; }
-    public string email { get; set; }
-    public int salary { get; set; }
+    
+    [Required, MinLength(5), MaxLength(13) ]
+     public string password { get; set; }
+     public string name { get; set; }
+     public string role { get; set; }
+     public string address { get; set; }
+     public string phone { get; set; }
+     public string email { get; set; }
+     public int salary { get; set; }
     
     public User(string userName, string password, string name, string role, string address, string phone, string email, int salary)
     {
