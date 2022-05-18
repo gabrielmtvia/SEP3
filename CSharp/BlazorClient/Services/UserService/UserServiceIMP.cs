151ba20586d54cfb9eae6c7de0c4ec19894c107d
hamoudi8080 <82207003+hamoudi8080@users.noreply.github.com>
2022-05-19 00:15:13 +0200

    user

--- CSharp/BlazorClient/Services/UserService/UserServiceIMP.cs
+++ CSharp/BlazorClient/Services/UserService/UserServiceIMP.cs
@@ -1,18 +1,34 @@
 
 
+using System.Net.Http.Headers;
+using System.Text.Json;
 using ModelClasses;
 
 namespace BlazorClient.Services.UserService;
 
 public class UserServiceIMP : IUserService
 {
+    
+    private readonly HttpClient httpClient;
+
+
+    public UserServiceIMP(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+    
     public async Task<User?> GetUserAsync(string username, string password)
     {
         
         
         ///// Here method to get the role then we create a user to send it to Authentication
+        User user = new User(username, password);
         
-        
+        var json = JsonSerializer.Serialize(user);
+        var buffer = System.Text.Encoding.UTF8.GetBytes(json);
+        var byteContent = new ByteArrayContent(buffer);
+        byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        await httpClient.PostAsync("/User", byteContent);
         
      // User user= new User(username,password,role);
         
