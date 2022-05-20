e975df827d5c253ae74234f7208079e8948c9433
TomaszGG <82205046+TomaszGGG@users.noreply.github.com>
2022-05-20 12:23:49 +0200

    added another field to OrdersDTO.cs, that will be needed in an order list in the employee's panel

--- CSharp/Shared/OrdersDTO.cs
+++ CSharp/Shared/OrdersDTO.cs
@@ -6,6 +6,16 @@ public class OrdersDTO
     public DateTime date { get; set; }
     public string status { get; set; }
     public string username { get; set; }
+    public UserDTO? user { get; set; } = null;
+
+    public OrdersDTO(long id, DateTime date, string status, string username, UserDTO? user)
+    {
+        this.id = id;
+        this.date = date;
+        this.status = status;
+        this.username = username;
+        this.user = user;
+    }
 
     public OrdersDTO(long id, DateTime date, string status, string username)
     {
