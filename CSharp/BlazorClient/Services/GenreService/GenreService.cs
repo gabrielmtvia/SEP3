8804aa99416129719087d1f77e31c0ca8973f500
elizaBianca <eliza.manciu66@gmail.com>
2022-05-23 14:03:29 +0200

    add book and add genre impl

--- CSharp/BlazorClient/Services/GenreService/GenreService.cs
+++ CSharp/BlazorClient/Services/GenreService/GenreService.cs
@@ -1,4 +1,7 @@
-﻿using ModelClasses;
+﻿using System.Text.Json;
+using ModelClasses;
+using JsonNamingPolicy = System.Text.Json.JsonNamingPolicy;
+using JsonSerializerOptions = System.Text.Json.JsonSerializerOptions;
 
 namespace BlazorClient.Services.GenreService;
 
@@ -18,5 +21,31 @@ public class GenreService : IGenreService
         var response = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Genre>>>("/Genre");
         if (response != null && response.Data != null)
             Genres = response.Data;
+    }
+
+    public async Task AddGenreAsync(Genre genreToAdd)
+    {
+        await _httpClient.PostAsJsonAsync("/Genre", genreToAdd);
+    }
+
+    public async Task DeleteGenreAsync(string type)
+    {
+        await _httpClient.DeleteAsync($"/Genre/{type}");
+    }
+
+    public async Task<List<Genre>> GetAllGenresAsync()
+    {
+        var result = await _httpClient.GetAsync("/Genre");
+        if (result.IsSuccessStatusCode)
+        {
+            var readAsStringAsync = await result.Content.ReadAsStringAsync();
+            var deserialize = JsonSerializer.Deserialize<List<Genre>>(readAsStringAsync, new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            return deserialize;
+        }
+
+        return null;
     }
 }
