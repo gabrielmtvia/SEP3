3450cc5ac183a163d092b4fa9c813415ee7533ee
hamoudi8080 <82207003+hamoudi8080@users.noreply.github.com>
2022-05-24 10:57:59 +0200

    view

--- CSharp/BlazorClient/Pages/EmployeeList.cs
+++ CSharp/BlazorClient/Pages/EmployeeList.cs
@@ -1,3 +1,4 @@
+using BlazorClient.Services.RegisterService;
 using Microsoft.AspNetCore.Components;
 using ModelClasses;
 
@@ -5,63 +6,36 @@ namespace BlazorClient.Pages;
 
 public class EmployeeList : ComponentBase
 {
+    [Inject] public NavigationManager NavigationManager { get; set; }
     public IEnumerable<UserDTO> Employees { get; set; }
+    
+    [Inject]
+    public IRegisterService IRegisterService{ get; set; }
 
     private string role = "Employee";
 
-    protected override Task OnInitializedAsync()
+    protected override async Task OnInitializedAsync()
     {
         try
         {
-            LoadEmployees();
-            return base.OnInitializedAsync();
+            Employees = await IRegisterService.GetUsersByRole(role);
+            
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
             throw;
+           
         }
-       
+        
+     
     }
 
-    private void LoadEmployees()
+   
+    
+    public void Delete()
     {
-        UserDTO e1 = new UserDTO()
-        {
-            userName = "Rojhat",
-            firstName = "rojhat12",
-            lastName = "efrini",
-            address = "efrin",
-            email = "efrin@outlook.com",
-            phone = "7114121",
-            password = "11111",
-
-
-        };
-        UserDTO e2 = new UserDTO()
-        {
-            userName = "Adam",
-            firstName = "rojhat12",
-            lastName = "kristen",
-            address = "efrin",
-            email = "efrin@outlook.com",
-            phone = "7114121",
-            password = "11111",
-
-
-        };
-        UserDTO e3 = new UserDTO()
-        {
-            userName = "Alex",
-            firstName = "rojhat12",
-            lastName = "muller",
-            address = "efrin",
-            email = "efrin@outlook.com",
-            phone = "7114121",
-            password = "11111",
-
-
-        };
-        Employees = new List<UserDTO>{e1, e2, e3};
+        
     }
+
 }
