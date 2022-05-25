5a5554dd253a36c340bc06ab0ce1877906b70321
elizaBianca <eliza.manciu66@gmail.com>
2022-05-25 12:02:31 +0200

    All books page created wih the view model logic

---
+++ CSharp/BlazorClient/Pages/AllBooksBase.cs
@@ -0,0 +1,45 @@
+﻿using BlazorClient.Services.BookService;
+using Microsoft.AspNetCore.Components;
+using ModelClasses;
+
+namespace BlazorClient.Pages;
+
+public class AllBooksBase : ComponentBase
+{
+    [Inject] public IBookService BookService { get; set; }
+    public List<Book> AllBooks { get; set; } = new();
+    public string SearchName = "";
+    [Inject] 
+    private NavigationManager navManager { get; set; }
+    public List<Book> DisplayBooks { get; set; } = new();
+
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        AllBooks = await BookService.GetAllBooksAsync();
+        DisplayBooks = AllBooks;
+    }
+
+    public async Task FilterBooksAsync()
+    {
+        DisplayBooks = AllBooks.Where(b => b.Title.ToLower().Contains(SearchName.ToLower())).ToList();
+    }
+
+    public async Task DeleteBookAsync(string isbn)
+    {
+        await BookService.DeleteBookAsync(isbn);
+    }
+
+    public async Task EditBookAsync(string isbn)
+    {
+       navManager.NavigateTo($"/AddBook/{isbn}");
+    }
+
+
+      
+    
+   
+}
+     
+    
